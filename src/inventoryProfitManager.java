3ff2103592bf113a9a276e2d0902630a44b61ba6
sanjayR-20 <sanjayr20606@gmail.com>
2026-06-13 11:31:02 +0530

    inventory manager to calculate the highest profit possible in a week

--- src/inventoryProfitManager.java
+++ src/inventoryProfitManager.java
@@ -1,29 +1,43 @@
 import java.util.*;
 
-
-
-public class inventoryProfitManager{
+public class inventoryProfitManager {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] arr = new int[6];
-        for(int i = 0; i < 6; i++){
+        int[] arr = new int[6];
+        System.out.println("Enter 6 daily prices:");
+        for (int i = 0; i < 6; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Prices: " + Arrays.toString(arr));
         logic(arr);
+        sc.close();
     }
-    public static void logic(int [] arr){
-        int lowest = arr[0];
-        int highest = arr[0];
-        for(int i = 1; i < 6; i++){
-            if(arr[i] < lowest){
-                lowest = arr[i];
-            }
-            else if (arr[i] > highest ){
-                highest = arr[i];
+
+    public static void logic(int[] arr) {
+        int minPrice = arr[0];
+        int maxProfit = 0;
+        
+        int buyDay = 1;  
+        int sellDay = 1; 
+        int tempBuyDay = 1; 
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            if (arr[i] < minPrice) {
+                minPrice = arr[i];
+                tempBuyDay = i + 1; 
+            } 
+            
+            else if (arr[i] - minPrice > maxProfit) {
+                maxProfit = arr[i] - minPrice;
+                buyDay = tempBuyDay;  
+                sellDay = i + 1;      
             }
         }
-        int profit = highest - lowest;
-        System.out.println("The Maximum profit is " + profit + ". " + "Where the item is bought on " + lowest + " day of the week and sold on " + highest + " day of the week");
+
+        
+        System.out.println("The Maximum profit is " + maxProfit + ". " + 
+                           "Where the item is bought on Day " + buyDay + 
+                           " and sold on Day " + sellDay + ".");
     }
 }
